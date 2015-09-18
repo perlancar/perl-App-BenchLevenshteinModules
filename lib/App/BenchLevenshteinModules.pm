fd1aa0cfbc2398556699a26ff5fec701abc3990c
perlancar (@pc-office) <perlancar@gmail.com>
2015-09-18 17:39:38 +0700

    Initial commit

---
+++ lib/App/BenchLevenshteinModules.pm
@@ -0,0 +1,24 @@
+package App::BenchLevenshteinModules;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(@modules);
+
+our @modules = (
+    # [module name, routine name]
+    ["PERLANCAR::Text::Levenshtein", "editdist"],
+    ["Text::Levenshtein", "fastdistance"],
+    ["Text::LevenshteinXS", "distance"],
+);
+
+1;
+# ABSTRACT: Benchmark various modules calculating the Levenshtein edit distance
+
+=head1 DESCRIPTION
